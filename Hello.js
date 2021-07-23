67b8a1f01bdcf8f327225b0b1d0d12eaca86760f
sinun <sinun.asdf@gmail.com>
2021-07-23 11:34:40 +0900

    master

---
+++ Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Hello() {
+    return <div>리엑트연습 hello!</div>
+}
+
+export default Hello;
---
+++ hello/src/App.js
@@ -0,0 +1,11 @@
+import './Hello';
+
+function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
